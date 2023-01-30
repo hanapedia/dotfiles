1d6b57fe317c94ecb44323bd0c9e280444506225
Hiroki <hiroki11hanada@gmail.com>
2023-01-30 13:43:02 +0900

    fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,53 @@
+# Theme
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # Theme
+    # set -g theme_color_scheme nord
+    set -g theme_color_scheme terminal2
+    set -g theme_display_date no
+    set -g theme_display_git_default_branch yes
+    set -g theme_display_k8s_context yes
+    set -g theme_newline_cursor yes
+    set -g theme_newline_prompt '$ '
+end
+
+# Path
+fish_add_path /opt/homebrew/bin
+fish_add_path $HOME/.cargo/bin
+source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+
+# Language version managers
+pyenv init - | source
+set -gx nvm_default_version v19.3.0
+
+# keybindings
+function fish_user_key_bindings
+  bind \cg __ghq_repository_search
+end
+
+# load aliases
+user_functions 
+user_functions_alias
+
+# default editor
+set -gx EDITOR "nvim"
+
+# run neovim in ide mode
+function nvi
+  nvim $argv --cmd "lua vim.g.IDE=true";
+end
+
+# run neovim in regular mode
+function nv
+  nvim $argv;
+end
+
+# zk
+set -gx ZK_EDITOR "nvim"
+
+## llvm
+set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
+
+## config git alias
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/.git --work-tree=$HOME'
---
+++ .config/fish/functions/user_functions_alias.fish
@@ -0,0 +1,14 @@
+function user_functions_alias
+end
+
+function bfg
+  java -jar ~/code/src/github.com/hanapedia/bfg/bfg.jar $argv;
+end
+
+function repo
+  cd ~/code/src/github.com/hanapedia/;
+end
+
+function lg
+  lazygit
+end
