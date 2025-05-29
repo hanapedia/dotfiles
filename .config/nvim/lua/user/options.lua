f85410388224ee7ac449706416fc3ee495076afa
Hiroki Hanada <hiroki-hanada@cybozu.co.jp>
2025-05-29 12:19:24 +0900

    tryout relative and absolute line numbers for mob

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -25,9 +25,7 @@ local options = {
   shiftwidth = 2, -- the number of spaces inserted for each indentation
   tabstop = 4, -- insert 4 spaces for a tab
   cursorline = true, -- highlight the current line
-  number = true, -- set numbered lines
-  relativenumber = true, -- set relative numbered lines
-  numberwidth = 4, -- set number column width to 2 {default 4}
+  numberwidth = 8, -- set number column width to 2 {default 4}
   signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
   wrap = true, -- display lines as one long line
   linebreak = true,
@@ -43,6 +41,11 @@ vim.opt.shortmess:append "c"
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
+
+-- show both relative and absolute line number
+vim.opt.nu = true -- set relative numbered lines
+vim.opt.relativenumber = true -- set relative numbered lines
+vim.o.statuscolumn = "%=%{printf('%3d', v:lnum)} %{printf('%2d', v:relnum)} %s│"
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
