5c851f341f1ea9461d691514c08960efebacf56c
Hiroki Hanada <hiroki-hanada@cybozu.co.jp>
2025-08-09 13:51:52 +0900

    add alias for nixos-config

--- .config/fish/functions/user_functions_alias.fish
+++ .config/fish/functions/user_functions_alias.fish
@@ -9,6 +9,10 @@ function repo
   cd ~/code/src/github.com/hanapedia/;
 end
 
+function nixconf
+  cd ~/code/src/github.com/hanapedia/nixos-config;
+end
+
 function vivi
   cd ~/code/src/github.com/vivianeinc/;
 end
